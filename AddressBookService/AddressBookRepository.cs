47fc9105aaeef9446b968d2be95082d1c4b5e34b
GollapudiLavanya <86302821+GollapudiLavanya@users.noreply.github.com>
2021-08-11 08:38:20 +0530

    Update AddressBookRepository.cs

--- AddressBookService/AddressBookRepository.cs
+++ AddressBookService/AddressBookRepository.cs
@@ -70,7 +70,7 @@ namespace AddressBookService
             try
             {
                 //Query to perform
-                string query = @"update AddressBookTable set Address='TekkaMitta' where FirstName='Sameera'";
+                string query = @"update AddressBookTable set Address='Nellore' where FirstName='Vindhya'";
                 SqlCommand cmd = new SqlCommand(query, this.connection);
                 this.connection.Open(); //Opening the connection
                 int result = cmd.ExecuteNonQuery();
